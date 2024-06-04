b7034e6c72c68ccfc87d330df67925ec31eae339
duda-kacper <kacper.duda@10g.pl>
2024-06-04 21:19:13 +0200

    html structure

---
+++ src/components/MainCard.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="main-container">
+        <div class="container-one">
+            <div class="profile">
+                <img src="../assets/Profile_image.jpg" alt="">
+                <h3>Kacper Duda</h3>
+                <p>Software tester</p>
+            </div>
+            <div class="social-links">
+                <a href="">
+                    <div class="link">
+                        <img src="../assets/GitHub.png" alt="">
+                        <h2>GitHub</h2>
+                    </div>
+                    <div class="link">
+                        <img src="../assets/Linkedin.png" alt="">
+                        <h2>Linkedin</h2>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="container-two">
+            <div class="about-me">
+                <h1>About me</h1>
+                <p>I am an ambitious, open to gain knowledge, young person who has experience working as a low-code platform tester. My enthusiasm and practice in the IT industry combined with theoreticalknowledge as well as passion for testing provide a solid basis for my development in this position.The ability to think analytically along with my scrupulousness in performing assigned tasks allowsme to thoroughly analyze requirements and detect potential errors. I can work in a team and have ahigh work culture. In my free time, I am interested in history and try to expand my professionalknowledge. In the future, I want to develop towards test automation.</p>
+            </div>
+            <div class="technology-section">
+                <h1>Technologies studied</h1>
+                <div class="logos">
+                    <img src="../assets/Postman.png" alt="">
+                    <img src="../assets/Postman.png" alt="">
+                    <img src="../assets/Postman.png" alt="">
+                </div>
+                <div class="button-container">
+                    <div class="projects-button">
+                        <a href="">My Projects</a>
+                    </div>
+                    <div>
+                        <a href="">My Resume</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style scoped>
+ img.
+ {
+
+ }
+
+</style>
